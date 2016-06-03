60b5fd1056c89870d22b642b3bddd86ab258cfc9
jaimiederijk <j.r.derijk@quicknet.nl>
2016-06-03 02:53:06 +0200

    and

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
+  $servername = "10.3.0.63";
+  $username = "jaapdzq3_jaimie";
   $password = "damcosecret";
-  $dbname = "damco";
+  $dbname = "jaapdzq3_damco";
 
   $currentDate = date("d-m-Y");
   $currentShortDate = date("Ymd");

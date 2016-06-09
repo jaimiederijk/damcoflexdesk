34279891fc8a358c6b40d9a15af88b75ed960589
jaimiederijk <j.r.derijk@quicknet.nl>
2016-06-09 18:31:00 +0200

    parser

--- index.php
+++ index.php
@@ -140,13 +140,43 @@
   }
   $numberOfDesk += $fixexdeskNotPresent;
         
-  
+  require 'php/class.iCalReader.php';
 
+  //$calenderJson2= @file_get_contents("https://calendar.google.com/calendar/ical/44ird5e4aofb9n5hjqb37ndgdo%40group.calendar.google.com/private-65a9ed8baeca92f0f21a491b24b0f2a5/basic.ics");
 
-// foreach($datePositions as $x => $x_value) {
-//                 echo "Key=" . $x . ", Value=" . $x_value;
-//                 echo "<br>";
-//             }
+  $ical   = new ICal("https://calendar.google.com/calendar/ical/44ird5e4aofb9n5hjqb37ndgdo%40group.calendar.google.com/private-65a9ed8baeca92f0f21a491b24b0f2a5/basic.ics");
+  $events = $ical->events();
+  $date = $events[0]['DTSTART'];
+  echo 'The ical date: ';
+  echo $date;
+  echo "<br />\n";
+  echo 'The Unix timestamp: ';
+  echo $ical->iCalDateToUnixTimestamp($date);
+  echo "<br />\n";
+  echo 'The number of events: ';
+  echo $ical->event_count;
+  echo "<br />\n";
+  echo 'The number of todos: ';
+  echo $ical->todo_count;
+  echo "<br />\n";
+  echo '<hr/><hr/>';
+  foreach ($events as $event) {
+      echo 'SUMMARY: ' . @$event['SUMMARY'] . "<br />\n";
+      echo 'DTSTART: ' . $event['DTSTART'] . ' - UNIX-Time: ' . $ical->iCalDateToUnixTimestamp($event['DTSTART']) . "<br />\n";
+      echo 'DTEND: ' . $event['DTEND'] . "<br />\n";
+      echo 'DTSTAMP: ' . $event['DTSTAMP'] . "<br />\n";
+      echo 'UID: ' . @$event['UID'] . "<br />\n";
+      echo 'CREATED: ' . @$event['CREATED'] . "<br />\n";
+      echo 'LAST-MODIFIED: ' . @$event['LAST-MODIFIED'] . "<br />\n";
+      echo 'DESCRIPTION: ' . @$event['DESCRIPTION'] . "<br />\n";
+      echo 'LOCATION: ' . @$event['LOCATION'] . "<br />\n";
+      echo 'SEQUENCE: ' . @$event['SEQUENCE'] . "<br />\n";
+      echo 'STATUS: ' . @$event['STATUS'] . "<br />\n";
+      echo 'TRANSP: ' . @$event['TRANSP'] . "<br />\n";
+      echo 'ORGANIZER: ' . @$event['ORGANIZER'] . "<br />\n";
+      echo 'ATTENDEE(S): ' . @$event['ATTENDEE'] . "<br />\n";
+      echo '<hr/>';
+  }
        
 ?>
 
